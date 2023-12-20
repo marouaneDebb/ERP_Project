e78eb937d21b0b18209f5d3b4cd82ca420db4bf2
marouane_debb <marwanedebb@gmail.com>
2023-12-21 00:53:03 +0100

    app

--- src/components/sidemenu/sidemenu.tsx
+++ src/components/sidemenu/sidemenu.tsx
@@ -21,9 +21,9 @@ const SideMenu: React.FC = () => {
         history.push("/sidemenu")
       }
       else if(rowId === "row_13"){
-        history.push("/sidemenu")
+        history.push("/parents")
       }
-      else if(rowId === "row-14"){
+      else if(rowId === "row_14"){
         history.push("/finance")
       }
       else if(rowId === "row_15"){
