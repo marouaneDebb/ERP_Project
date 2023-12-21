d579d74d9cb1f28409c89eea9396997f777fd4d9
marouane_debb <marwanedebb@gmail.com>
2023-12-21 03:10:59 +0100

    Updates

--- assets/md.transition-legacy-eTkw_uN2.js
+++ assets/md.transition-legacy-eTkw_uN2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BcT9VrfY.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
+System.register(["./index-legacy-VQ8huYut.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
