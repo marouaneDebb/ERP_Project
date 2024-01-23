b6e3432ffd7649081a16ef5e8da84ebe041d5b75
Youssef Oudourouch <youdourouch@gmail.com>
2024-01-23 13:08:42 +0100

    payment

--- src/components/finance/payments/payment/payment.tsx
+++ src/components/finance/payments/payment/payment.tsx
@@ -1,4 +1,5 @@
 import { IonCol, IonRow } from "@ionic/react";
+import { useState } from "react";
 
 interface Payment{
     id:number;
@@ -11,16 +12,45 @@ interface Payment{
 }
 interface props {
     payment: Payment;
-  }
+    updatePrice(price:number):void;
+    total:number;
+    //updateList(payment:Payment[]):void;
+    //selectedPayment:Payment[];
+}
 
-const ShowPayment: React.FC <props>= ({ payment }) => {
+const ShowPayment: React.FC <props>= ({ payment,updatePrice,total}) => {
+  const [isChecked, setIsChecked] = useState<boolean>(payment.type === "Obligatory");
+  let buffer1:Payment[]=[]
+
+  const handleCheckboxChange = () => {
+    let prix=total;
+    setIsChecked(!isChecked);
+    //buffer1=selectedPayment;
+    if(!isChecked){
+        buffer1.push(payment);
+        prix+=payment.price;
+    }
+    else{
+        buffer1 = buffer1.filter(paiment => paiment.id !== payment.id);
+        prix-=payment.price;
+    }
+    //updateList(buffer1);
+    updatePrice(prix);
+    console.log(prix);
+  };
+
+  
     return(
-        <div>
+        <div className="my-2">
             <IonRow>
-                <IonCol><input type="checkbox" /></IonCol>
+                <IonCol><input type="checkbox"
+                    checked={isChecked}
+                    disabled={payment.type === "Obligatory"}
+                    onChange={handleCheckboxChange}
+                /></IonCol>
                 <IonCol>{payment.name}</IonCol>
-                <IonCol>{payment.dateBegin}</IonCol>
-                <IonCol>{payment.dateEnd}</IonCol>
+                <IonCol className="subTitle">{payment.dateBegin}</IonCol>
+                <IonCol className="subTitle">{payment.dateEnd}</IonCol>
                 <IonCol>{payment.childName}</IonCol>
                 <IonCol>{payment.price}</IonCol>
                 <IonCol>{payment.type}</IonCol>
--- src/components/finance/payments/payments.tsx
+++ src/components/finance/payments/payments.tsx
@@ -1,9 +1,10 @@
-import { IonButton, IonSearchbar } from "@ionic/react";
+import { IonButton, IonCol, IonRow, IonSearchbar } from "@ionic/react";
 import Notification_setting from "../../notification_setting";
 import SideMenu from "../../sidemenu/sidemenu";
 import ListWithPagination from "../../page/pages";
 import ShowPayment from "./payment/payment";
 import "./payments.css"
+import { useEffect, useState } from "react";
 
 
 interface Payment{
@@ -17,18 +18,33 @@ interface Payment{
 }
 
 function Payments(){
-    let items=[{id:1, name:"gym", dateBegin:"12/03/2024",dateEnd:"12/04/2024",childName:"ana",price:100, type:"Optional"},
+    let items=[{id:1, name:"gym", dateBegin:"12/03/2024",dateEnd:"12/04/2024",childName:"ana",price:100, type:"Obligatory"},
     {id:2, name:"transport", dateBegin:"12/03/2024",dateEnd:"12/04/2024",childName:"ana",price:100, type:"Optional"},
     {id:3, name:"gym", dateBegin:"12/03/2024",dateEnd:"12/04/2024",childName:"howa",price:100, type:"Optional"},
-    {id:4, name:"transport", dateBegin:"12/03/2024",dateEnd:"12/04/2024",childName:"hia",price:100, type:"Optional"}];
-    let prix=0;
+    {id:4, name:"transport", dateBegin:"12/03/2024",dateEnd:"12/04/2024",childName:"hia",price:100, type:"Obligatory"}];
+    
+    const [isChecked, setIsChecked] = useState(false);
+    let [prix, setPrix]=useState(0);
+    let [selectedPayment,setSelectedPayment] = useState<Payment[]>([]);
+    
+    useEffect(() => {
+      let buffer: Payment[] = [];
+      let prices=0;
+  
+      items.forEach((payment) => {
+        if (payment.type === 'Obligatory') {
+          buffer.push(payment);
+          prices+=payment.price;
+        }
+      });
+      setSelectedPayment(buffer);
+      setPrix(prices);
+    }, []);
+    
     return(
       <div className="bgroundpayment row">
         <SideMenu active_Row={"row_13"}/>
-        
-        <div className="medSection col-10">
-          
-          
+        <div className="medSection col-10">  
           <div className="col-12 midle1 p-3">
                 <div className="service_title row">
                     <div className="service_title_text col-3">Payments</div>
@@ -39,29 +55,55 @@ function Payments(){
                         <Notification_setting />
                     </div>
                 </div>
+
                 <div className="formulaire">
-                    <div className="newAccount row">
-                        <h5 className="col-10"> Payments</h5>
+                    <div className="newAccount">
+                      <IonRow>
+                        <h5 className="col-9"> Payments</h5>
                         <h5 className="col-2">{prix} DH</h5>
-
+                        </IonRow>
                     </div>
-                    <div className="mainf row">
-            
-                        <ListWithPagination<Payment>
-                              itemsPerPage={8}
-                              data={items}
-                              renderListItem={(payment) => (
-                                    <ShowPayment key={payment.id} payment={payment} />
-                                  
-                              )}
-                              />
-                             
-          </div>
+                    
+                    <div className="my-2 mx-4">
+                      <IonRow >
+                          <IonCol>
+                            <input type="checkbox"
+                                checked={isChecked}
+                             /> All
+                          </IonCol>
+                          <IonCol>Service name</IonCol>
+                          <IonCol >Begin date</IonCol>
+                          <IonCol >End date</IonCol>
+                          <IonCol>child name</IonCol>
+                          <IonCol>Price</IonCol>
+                          <IonCol>Type</IonCol>
+                      </IonRow>
+                      <hr className="mb-4"></hr>
+                      <ListWithPagination<Payment>
+                            itemsPerPage={8}
+                            data={items}
+                            renderListItem={(payment) => (
+                                  <ShowPayment 
+                                    key={payment.id} 
+                                    payment={payment}
+                                    updatePrice={setPrix} 
+                                    //updateList={setSelectedPayment}
+                                    total={prix}
+                                    //selectedPayment={selectedPayment}
+                                  />
+                                
+                            )}
+                            />
                         
+                        <div className="button-container mt-2">
+                            <IonButton shape="round" className="text_1">
+                                Pay
+                            </IonButton>
+                        </div>
                     </div>
+                </div>
 
-
-          </div>
+              </div>
         </div>
       </div>
     )
