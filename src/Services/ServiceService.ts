9c905d3a33b56cbfea227e0c222d3ea8eef22cab
Tarik-3 <tarikaitahmid@gmail.com>
2024-02-14 08:48:55 +0100

    Adding a service and resolve the problem of creating a student

--- src/Services/ServiceService.ts
+++ src/Services/ServiceService.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 import { URL_BACK } from "./StrudentService";
     
-const creatService = (service:any)=> axios.post(URL_BACK+"/service/add",service) 
+export const createService = (service:any)=> axios(URL_BACK+"/service/add",service) 
--- src/components/service/NewService.tsx
+++ src/components/service/NewService.tsx
@@ -14,35 +14,56 @@ import {
   IonIcon,
 } from "@ionic/react";
 import { add } from "ionicons/icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./NewService.css";
 import NewDiscount from "../Discount/newDiscount";
+import { createService } from "../../Services/ServiceService";
+import { getAllDiscount } from "../../Services/DiscountService";
+// import { getAllDiscount } from "../../Services/discounts";
 
 
 
 function NewService() {
 
-
+interface discountType {
+  nameDiscount:String;
+  description:String;
+  conditionn:String;
+  dateValidation:String;
+  percentage:String;
+}
   const [selectedOption, setSelectedOption] = useState<string | undefined>(
     undefined
   );
   const [discount, setDiscount] = useState(false);
+  const [discounts, setDiscounts] = useState<discountType[]>([]);
+  const [discountsExist, setDiscountsExist] = useState<discountType[]>([]);
+  const handleSelectChange = (e:any) => {
+    const selectedIndex = e.target.value;
+    setSelectedOption(selectedIndex);
+
+    // Get the selected discount object based on the index
+    const selectedDiscount = discountsExist[selectedIndex];
+    // const discountJson = JSON.stringify(selectedDiscount)
+    // Add the selected discount to the discountCh list
+    setDiscounts([...discounts, selectedDiscount]);
+  };
+
 
   const newDiscount = () => {
     setDiscount(!discount);
   };
 
   //Mapping
-  const [nameService,setNameService] = useState('');
+  const [name,setName] = useState('');
   const [start,setStart] = useState('');
   const [description,setDescription] = useState('');
-//   const [discounts,setDiscounts] = useState('');
-  const [periode,setPeriode] = useState('');
+  const [pereodicity,setPereodicity] = useState('');
   const [price,setPrice] = useState('');
   const [type,setType] = useState('');
 
-  const handleNameServiceChange = (e: any) => {
-    setNameService(e.target.value);
+  const handlenameChange = (e: any) => {
+    setName(e.target.value);
   };
   const handleStartChange = (e: any) => {
     setStart(e.target.value);
@@ -50,8 +71,8 @@ function NewService() {
   const handleDescChange = (e: any) => {
     setDescription(e.target.value);
   };
-  const handlePeriodeChange = (e: any) => {
-    setPeriode(e.target.value);
+  const handlepereodicityChange = (e: any) => {
+    setPereodicity(e.target.value);
   };
   const handlePriceChange = (e: any) => {
     setPrice(e.detail.value);
@@ -64,18 +85,56 @@ function NewService() {
 
   const saveService= ()=>{
     const service = {
-        nameService,
+        name,
         start,
-        periode,
+        pereodicity,
         price,
         type,
-        discount,
-        description
+        description,
+        discounts
     }
+    console.log(service,"service data")
+    const discJson:any =[]
+    discounts.map((disc) =>{
+        discJson.push(JSON.stringify({disc}))
+    })
+    // console.log(discJson, "disc Json")
 
     const serviceJson = JSON.stringify(service);
+
+    const requestOptions = {
+      method: "POST",
+      url: "http://localhost:8090/service/add",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: serviceJson,
+    };
     console.log(serviceJson, "service data");
+    console.log(requestOptions, "service data");
+   
+    
+    createService(requestOptions).then((res) =>{
+      console.log(res.data)
+    }).catch((error) => {
+      console.error(
+        "Une erreur s'est produite lors de la création de service:",
+        error
+      );
+    });
+    // console.log(discounts,"choisi disc")
   }
+
+  //discount
+  useEffect(()=>{
+    getAllDiscount()
+    .then((res)=>{
+      setDiscountsExist(res.data)
+      console.log(res.data,"all discount")
+      
+    })
+  },
+    [])
 
 
   return (
@@ -94,8 +153,8 @@ function NewService() {
                   className="my-2"
                   fill="outline"
                   type="text"
-                  value={nameService}
-                  onIonChange={handleNameServiceChange}
+                  value={name}
+                  onIonChange={handlenameChange}
                 ></IonInput>
               </div>
               <div>
@@ -118,11 +177,16 @@ function NewService() {
                 fill="outline"
                 placeholder="choose a discount"
                 value={selectedOption}
-                onIonChange={(e) => setSelectedOption(e.detail.value)}
+                onIonChange={handleSelectChange}
               >
-                <IonSelectOption value="option1">Option 1</IonSelectOption>
-                <IonSelectOption value="option2">Option 2</IonSelectOption>
-                <IonSelectOption value="option3">Option 3</IonSelectOption>
+                {discountsExist.map((disc,index) =>(
+                  <IonSelectOption key={index} value={index}>{disc.nameDiscount}</IonSelectOption>
+
+                )
+                )}
+                
+                {/* <IonSelectOption value="option2">Option 2</IonSelectOption>
+                <IonSelectOption value="option3">Option 3</IonSelectOption> */}
               </IonSelect>
             </div>
             <div className="col-5 mx-3">
@@ -131,9 +195,9 @@ function NewService() {
                 fill="outline"
                 type="number"
                 className="my-2"
-                onIonChange={handlePeriodeChange}
+                onIonChange={handlepereodicityChange}
 
-                value={periode}
+                value={pereodicity}
                 required
               ></IonInput>
               <IonLabel>Price(DH)*</IonLabel>
@@ -148,11 +212,11 @@ function NewService() {
               ></IonInput>
               <IonLabel>Type*</IonLabel>
               <div className="tipe">
-                <IonRadioGroup className="my-2" onChange={handleTypeChange}>
-                  <IonRadio className="col-4 mx-5" >
+                <IonRadioGroup className="my-2" value={type} onIonChange={handleTypeChange}>
+                  <IonRadio className="col-4 mx-5" value="obligatory" >
                     Obligatory
                   </IonRadio>
-                  <IonRadio className="col-4" >
+                  <IonRadio className="col-4" value="optional">
                     Optional
                   </IonRadio>
                 </IonRadioGroup>
